b32e9384bd08c50c25fbfeafb0dbdb8b26eb6858
ngcsontrh <trhsonngoc12@gmail.com>
2025-06-10 05:27:36 +0700

    up

--- RStudentManagement/Components/SidebarControl.Designer.cs
+++ RStudentManagement/Components/SidebarControl.Designer.cs
@@ -31,7 +31,6 @@
             label1 = new Label();
             button1 = new Button();
             button2 = new Button();
-            button4 = new Button();
             SuspendLayout();
             // 
             // label1
@@ -67,22 +66,11 @@
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
-            // button4
-            // 
-            button4.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button4.Location = new Point(9, 266);
-            button4.Name = "button4";
-            button4.Size = new Size(208, 38);
-            button4.TabIndex = 7;
-            button4.Text = "Gửi email";
-            button4.UseVisualStyleBackColor = true;
-            // 
             // SidebarControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.MediumSeaGreen;
-            Controls.Add(button4);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(label1);
@@ -96,6 +84,5 @@
         private Label label1;
         private Button button1;
         private Button button2;
-        private Button button4;
     }
 }
