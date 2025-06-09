afd471f7054e30563882a09c86a10d01655fe39a
ngcsontrh <trhsonngoc12@gmail.com>
2025-06-10 05:47:14 +0700

    up

---
+++ RStudentManagement/Entities/EventLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RStudentManagement.Entities
+{
+    public class EventLog
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string EventType { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public Guid? UserId { get; set; }
+    }
+}
